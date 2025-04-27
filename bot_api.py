292860685eef7ae4d9872b953190fa8ad192fe83
ramsvikyolo <49155620+ramsvikyolo@users.noreply.github.com>
2025-04-27 23:50:50 +0200

    Update bot_api.py

--- bot_api.py
+++ bot_api.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import requests
 import json
@@ -48,4 +49,5 @@ def webhook():
     return 'OK'
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT, default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
